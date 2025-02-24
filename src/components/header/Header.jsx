b0b17beadd30aa1ec633c13c4d3220ec7bef4ba4
mehditayefe <mehditayefe1996@gmail.com>
2025-02-24 19:10:42 +0330

    crate header components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import cartImg from "../../assets/cart.png"
+import userImg from "../../assets/user.png"
+import searchImg from "../../assets/search.png"
+
+function Header() {
+  return (
+    <div className="w-full h-11 p-10 flex  justify-between items-center  top-0">
+      <div><h1 className="font-black text-2xl cursor-pointer">SHOPPE</h1></div>
+      <div className="w-1/3 flex justify-between items-center">
+        <div className="w-1/2 flex justify-evenly items-center ml-10">
+          <p className="mr-7 cursor-pointer">shop</p>
+          <p className="mr-7 cursor-pointer">blog</p>
+          <p className="mr-7 cursor-pointer">our story</p>
+          <p><span className="cursor-none">|</span></p>
+        </div>
+        <div className="w-1/3 flex justify-start items-center flex-row-reverse">
+            <div className="w-10 h-10 flex justify-center items-center ml-5 cursor-pointer"><img src={userImg}></img></div>
+            <div className="w-6 h-6 flex justify-center items-center ml-5 cursor-pointer"><img src={cartImg}></img></div>
+            <div className="w-10 h-10 flex justify-center items-center ml-5 cursor-pointer"><img src={searchImg}></img></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
