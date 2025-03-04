d5354b7edfb60cc60d3b61ba6dc8456ff2a455f8
mehditayefe <mehditayefe1996@gmail.com>
2025-03-04 10:30:16 +0330

    create footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
-
+import pointerImg from "../../assets/pointer.png"
+import facebookImg from "../../assets/facebook.png"
+import linkdinImg from "../../assets/linkdin.png"
+import instagramImg from "../../assets/instagram.png"
+import xImg from '../../assets/x.png'
 
 function Footer() {
   return (
-    <div>
-      footer
+    <div className="w-full h-22 p-10 fixed bottom-20 ">    
+       <div className="w-full h-max flex justify-between items-center mb-5">
+          <div className="w-max flex justify-start items-center">
+              <p className="cursor-pointer mr-7">CONTACT</p>
+              <p className="cursor-pointer mr-7">TERMS OF SERVICES</p>
+              <p className="cursor-pointer mr-7">SHIPPING AND RETURNS</p>
+          </div>
+          <div className="w-max h-11 flex justify-between items-center border-b-2 mb-5">
+            <p className="cursor-pointer mr-11">Give an email, get the newsletter.</p>
+            <img src={pointerImg}></img>
+          </div>
+       </div> 
+       <div className="w-full flex justify-between items-center">
+          <p>© 2021 Shelly. Terms of use and privacy policy.</p>
+          <div className="w-max flex justify-start items-center">
+              <img className="mr-7" src={linkdinImg}></img>
+              <img className="mr-7" src={facebookImg}></img>
+              <img className="mr-7" src={instagramImg}></img>
+              <img className="mr-7" src={xImg}></img>
+          </div>
+       </div>
     </div>
   )
 }
