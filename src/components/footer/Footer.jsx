31d58ca514624bed74f775e7082e36a89b26f413
mehditayefe <mehditayefe1996@gmail.com>
2025-03-19 02:17:15 +0330

    fix footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,27 +7,25 @@ import { Link } from "react-router-dom"
 
 function Footer() {
   return (
-    <div className="w-full h-15 px-10 bottom-20 ">
-      <div className="w-full h-15 px-10  border-t-2  ">
-        <div className="w-full h-max flex justify-between items-center mb-5">
-          <div className="w-max flex justify-start items-center">
-            <Link to={"/contact"} className="cursor-pointer mr-7">CONTACT</Link>
-            <Link to={"/Privacy"} className="cursor-pointer mr-7">TERMS OF SERVICES</Link>
-            <p className="cursor-pointer mr-7">SHIPPING AND RETURNS</p>
-          </div>
-          <div className="w-max h-11 flex justify-between items-center border-b-2 mb-5">
-            <p className="cursor-pointer mr-11">Give an email, get the newsletter.</p>
-            <img src={pointerImg}></img>
-          </div>
+    <div className="w-full h-40 px-10 flex flex-col justify-start items-start border-t-2 border-gray-200">
+      <div className="w-full h-max flex justify-between items-center mb-5">
+        <div className="w-max h-full flex justify-start items-center">
+          <Link to={"/contact"} className="cursor-pointer mr-7">CONTACT</Link>
+          <Link to={"/Privacy"} className="cursor-pointer mr-7">TERMS OF SERVICES</Link>
+          <p className="cursor-pointer mr-7">SHIPPING AND RETURNS</p>
         </div>
-        <div className="w-full flex justify-between items-center">
-          <p>© 2021 Shelly. Terms of use and privacy policy.</p>
-          <div className="w-max flex justify-start items-center">
-            <img className="mr-7 cursor-pointer" src={linkdinImg}></img>
-            <img className="mr-7 cursor-pointer" src={facebookImg}></img>
-            <img className="mr-7 cursor-pointer" src={instagramImg}></img>
-            <img className="mr-7 cursor-pointer" src={xImg}></img>
-          </div>
+        <div className="w-max h-11 flex justify-between items-center border-b-2 mb-5">
+          <p className="cursor-pointer mr-11">Give an email, get the newsletter.</p>
+          <img src={pointerImg}></img>
+        </div>
+      </div>
+      <div className="w-full h-max flex justify-between items-center mb-5">
+        <p>© 2021 Shelly. Terms of use and privacy policy.</p>
+        <div className="w-max flex justify-start items-center">
+          <img className="mr-7 cursor-pointer" src={linkdinImg} />
+          <img className="mr-7 cursor-pointer" src={facebookImg} />
+          <img className="mr-7 cursor-pointer" src={instagramImg} />
+          <img className="mr-7 cursor-pointer" src={xImg} />
         </div>
       </div>
     </div>
