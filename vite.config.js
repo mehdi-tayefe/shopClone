bf9bca761daa9df2f58d9684987869fc99cbd1f5
mehditayefe <mehditayefe1996@gmail.com>
2025-04-09 16:12:06 +0330

    deploy with git pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base:"/react-shop/" ,
   plugins: [
     tailwindcss(),
     react(),
