0297b5f3c2bc2fddaa92a27badc89a46813d2a7c
mehditayefe <mehditayefe1996@gmail.com>
2025-04-09 17:04:32 +0330

    add base name to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/shopClone">
       <App />
     </BrowserRouter>
   </StrictMode>,
